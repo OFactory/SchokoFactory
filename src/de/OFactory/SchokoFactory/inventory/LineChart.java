a0a599f0afe86b56a6acc9e3a4f9c12bb0d3d7f0
Hans Olischläger <h.olischlaeger@gmx.de>
2016-12-14 15:42:30 +0100

    LineChart und MarketInfoTab

---
+++ src/de/OFactory/SchokoFactory/inventory/LineChart.java
@@ -0,0 +1,88 @@
+package de.OFactory.SchokoFactory.inventory;
+
+import java.util.ArrayList;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+
+import de.OFactory.SchokoFactory.main.Drawable;
+import de.OFactory.SchokoFactory.main.Updateable;
+
+public class LineChart implements Updateable, Drawable{
+
+	int x;
+	int y;
+	int width;
+	protected int height;
+	
+	protected Shape shape;
+	
+	ArrayList<Integer> points = new ArrayList<Integer>();
+	
+	
+	public LineChart(int x, int y, int width, int height){
+		
+		
+		this.x = x;
+		this.y = y;
+		
+		this.width = width;
+		this.height = height;
+		
+		this.shape = new Rectangle(x, y, width, height);
+	}
+	
+	public void draw(Graphics g) {
+
+		g.draw(this.shape);
+		g.setColor(Color.blue);
+		g.setLineWidth(2);
+		if (points.size() > 0) {
+			int max = getMax(points);
+			int px = 0;
+			int apx = 0;
+			int apy = (int)((float)points.get(0)/max*this.height * 0.9);
+			int dx = (int)((float)this.width/points.size());
+			if (dx == 0) {
+				dx = 1;
+			}		
+			
+			for (int py:points) {
+				
+				px = px + dx;
+
+				py = (int)((float)py/max * this.height * 0.9);
+				g.drawLine(this.x + apx, this.y + this.height - apy, this.x + px, this.y + this.height - py);
+				
+				apx = px;
+				apy = py;
+				
+			}
+		}
+		g.setLineWidth(1);
+	}
+
+	public void update(GameContainer gc) {
+
+		this.shape = new Rectangle(this.x, this.y, this.width, this.height);
+		
+	}
+	
+	public void addPoint(int value) {
+		points.add(value);
+
+	}
+	
+	public int getMax(ArrayList<Integer> list){
+	    int max = Integer.MIN_VALUE;
+	    for(int i=0; i<list.size(); i++){
+	        if(list.get(i) > max){
+	            max = list.get(i);
+	        }
+	    }
+	    return max;
+	}
+}
